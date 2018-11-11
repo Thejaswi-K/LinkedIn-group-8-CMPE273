2e213065d3d212180dcdfd4ea0fffb739cc6df84
Mithun Harikumar Nair <mithun.harikumarnair@sjsu.edu>
2018-11-11 15:28:03 -0800

    schema

---
+++ Backend/Model/Recruiter.js
@@ -0,0 +1,87 @@
+var mongoose = require("mongoose");
+var utility = require("../utility");
+
+var recruiterSchema = new mongoose.Schema({
+  _id: mongoose.Schema.Types.ObjectId,
+  firstName: {
+    type: String,
+    required: true,
+    set: utility.capitalizeFirstLetter,
+    default: ""
+  },
+  lastName: {
+    type: String,
+    required: true,
+    set: utility.capitalizeFirstLetter,
+    default: ""
+  },
+  email: {
+    type: String,
+    // set: utility.toLower,
+    lowercase: true,
+    required: true,
+    default: ""
+  },
+  password: {
+    type: String,
+    required: true
+  },
+
+  phoneNumber: {
+    type: Number,
+    required: false,
+    get: v => Math.round(v),
+    default: ""
+  },
+  address: {
+    type: String,
+    required: false,
+    set: utility.capitalizeFirstLetter,
+    default: ""
+  },
+  city: {
+    type: String,
+    required: false,
+    set: utility.capitalizeFirstLetter,
+    default: ""
+  },
+  state: {
+    type: String,
+    required: false,
+    set: utility.capitalizeFirstLetter,
+    default: ""
+  },
+  zipcode: {
+    type: Number,
+    required: false,
+    get: v => Math.round(v),
+    default: ""
+  },
+  
+  companyName: {
+    type: String,
+    required: false,
+    default: ""
+  },
+  profileimage: {
+    type: String,
+    required: false,
+    default: ""
+  },
+  gender: {
+    type: String,
+    required: false,
+    set: utility.capitalizeFirstLetter,
+    default: ""
+  },
+  memberSince: {
+    type: Date,
+    default: Date.now
+  },
+  isRecruiter: {
+    type: Boolean,
+    default: true
+  }
+});
+
+module.exports = mongoose.model("Recruiters", recruiterSchema);
