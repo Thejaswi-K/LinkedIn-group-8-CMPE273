eed3c231ec1b076c32efa4de4f1c192b47586db5
Akshat Goel <akshatishu@gmail.com>
2018-11-14 14:30:25 -0800

    Working on applicant view profile.

---
+++ kafka-backend/services/applicants/applicantViewProfile.js
@@ -0,0 +1,31 @@
+// Load Property model
+const ApplicantUser = require('../../Model/Applicant');
+
+function handle_request(msg, callback) {
+
+
+    var res = {};
+
+    ApplicantUser.find({_id: msg.applicant_id})
+        .then(profile => {
+            if (!profile) {
+                res.code = 404 ;
+                res.message = "User not found" ;
+                callback(null,res);
+
+            }
+
+            res.code = 200 ;
+            res.message = profile ;
+            callback(null,res);
+        })
+        .catch(function (err) {
+            res.message = err;
+            res.code = 400;
+            callback(null, res);
+        });
+
+    console.log("after callback" + res);
+};
+
+exports.handle_request = handle_request;
