2975c4f7e4e1a474689981357552f430f7f2b1ad
Akshat Goel <akshatishu@gmail.com>
2018-11-17 16:27:56 -0800

    Deleted applicant model id from backend.

--- Backend/Model/Applicant.js
+++ Backend/Model/Applicant.js
@@ -2,7 +2,7 @@ var mongoose = require("mongoose");
 var utility = require("../utility");
 
 var applicantSchema = new mongoose.Schema({
-  _id: mongoose.Schema.Types.ObjectId,
+
   firstName: {
     type: String,
     required: true,
