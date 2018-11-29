33846f81d9b6b4e2e014e7fd5cfca50025088515
Thejaswi Kampalli <thejaswi.kampalli@sjsu.edu>
2018-11-29 15:18:08 -0800

    new JobDetails.js after conflict

--- frontend/src/components/Main.js
+++ frontend/src/components/Main.js
@@ -17,7 +17,7 @@ import store from "../store";
 import MainRecruiterDashboard from "./Recruiter/RecruiterDashboard/main";
 import ViewConnections from './Connections/ViewConnections';
 import ViewPendingRequests from './Connections/ViewPendingRequests'
-import JobDetails from './Job/Details';
+import JobDetails from './Job/jobDetails';
 import JobApply from './Job/jobapply';
 
 // Create a Main Component
