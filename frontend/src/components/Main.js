a7da55a5974abf04efb7da6f90435bac3eae2249
akshat goel <akshatishu@gmail.com>
2018-12-04 03:53:51 -0800

    Added recruiter profile view only.

--- frontend/src/components/Main.js
+++ frontend/src/components/Main.js
@@ -14,6 +14,7 @@ import ApplicantSignup from "./Applicant/ApplicantSignup/ApplicantSignup";
 import RecruiterSignup from "./Recruiter/RecruiterSignup/RecruiterSignup";
 import ApplicantProfileView from "./Applicant/ApplicantProfile/ApplicantProfileView";
 import ApplicantProfileViewOnly from "./Applicant/ApplicantProfileViewOnly/ApplicantProfileViewOnly";
+import RecruiterProfileViewOnly from "./Recruiter/RecruiterProfileViewOnly/RecruiterProfileViewOnly";
 import RecruiterProfileView from "./Recruiter/RecruiterProfile/RecruiterProfileView";
 import Provider from "react-redux/es/components/Provider";
 import store from "../store";
@@ -53,6 +54,10 @@ class Main extends Component {
                     <Route
                         path="/applicantprofileviewonly"
                         component={ApplicantProfileViewOnly}
+                    />
+                    <Route
+                        path="/recruiterprofileviewonly"
+                        component={RecruiterProfileViewOnly}
                     />
                     <Route
                         path="/recruiterprofileview"
